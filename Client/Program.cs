4d187f22c7b8f354b523fb5cc6437d97e06229e7
Dmitri Mogilevski <dmogilevski@gmail.com>
2019-08-04 16:59:17 -0500

    ClientCredentials auth works.

--- Client/Program.cs
+++ Client/Program.cs
@@ -39,7 +39,7 @@ namespace Client
 
 			client.SetBearerToken(tokenResponse.AccessToken);
 
-			var response = await client.GetAsync("http://localhost:18080/jobs");
+			var response = await client.GetAsync("http://localhost:18080/job");
 			if (!response.IsSuccessStatusCode)
 			{
 				Console.WriteLine(response.StatusCode);
--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -1,8 +1,4 @@
-﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
-
-
-using IdentityServer4.Models;
+﻿using IdentityServer4.Models;
 using System.Collections.Generic;
 
 namespace IdentityServer
@@ -24,7 +20,11 @@ namespace IdentityServer
 				new ApiResource
 				{
 					Name = "securedWebApi",
-					DisplayName = "Secured Web API"
+					DisplayName = "Secured Web API",
+					Scopes = new List<Scope>
+					{
+						new Scope("securedWebApi")
+					}
 				}
 			};
 		}
@@ -36,15 +36,14 @@ namespace IdentityServer
 				new Client
 				{
 					ClientId = "client",
-
-					// no interactive user, use the clientid/secret for authentication
-					AllowedGrantTypes = GrantTypes.ClientCredentials,
-
 					// secret for authentication
 					ClientSecrets =
 					{
 						new Secret("secret".Sha256())
 					},
+
+					// no interactive user, use the clientid/secret for authentication
+					AllowedGrantTypes = GrantTypes.ClientCredentials,
 
 					// scopes that client has access to
 					AllowedScopes = {"securedWebApi" }
