a87efd1a649f55ec71c43b84abb29247d9adc48b
Dmitri Mogilevski <dmogilevski@gmail.com>
2019-08-04 16:40:58 -0500

    API and Client proj added.

---
+++ Api/Controllers/JobController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+	[Route("job")]
+	[Authorize]
+	public class JobController : ControllerBase
+	{
+		[HttpGet]
+		public async Task<ActionResult> Get()
+		{
+			return new JsonResult(from c in User.Claims select new {c.Type, c.Value});
+		}
+	}
+}
