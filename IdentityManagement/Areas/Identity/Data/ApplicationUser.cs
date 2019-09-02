b675053fc6e3f45363a85273b5aeb500cdda8d1a
Dmitri Mogilevski <dmogilevski@gmail.com>
2019-09-02 13:44:32 -0500

    IdentityManagement with custom ApplicationUser class.

---
+++ IdentityManagement/Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace IdentityManagement.Areas.Identity.Data
+{
+	// Add profile data for application users by adding properties to the ApplicationUser class
+	public class ApplicationUser : IdentityUser
+	{
+		[PersonalData]
+		public DateTime DOB { get; set; }
+	}
+}
---
+++ IdentityManagement/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿//using IdentityManagement.Areas.Identity.Data;
+//using IdentityManagement.Data;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.AspNetCore.Identity;
+//using Microsoft.AspNetCore.Identity.UI;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.DependencyInjection;
+//
+//[assembly: HostingStartup(typeof(IdentityManagement.Areas.Identity.IdentityHostingStartup))]
+//namespace IdentityManagement.Areas.Identity
+//{
+//	public class IdentityHostingStartup : IHostingStartup
+//    {
+//        public void Configure(IWebHostBuilder builder)
+//        {
+//            builder.ConfigureServices((context, services) => {
+//                services.AddDbContext<ApplicationDbContext>(options =>
+//                    options.UseSqlServer(
+//                        context.Configuration.GetConnectionString("DefaultConnection")));
+//
+//                services.AddDefaultIdentity<ApplicationUser>()
+//	                .AddDefaultUI(UIFramework.Bootstrap4)
+//					.AddEntityFrameworkStores<ApplicationDbContext>();
+//            });
+//        }
+//    }
+//}
